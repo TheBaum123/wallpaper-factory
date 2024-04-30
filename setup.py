28c4e123b1cb427d87453dcbd3c5976936c41a63
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2024-04-30 15:19:07 +0200

    update package distribution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = f.read()
 
 setup(
     name="wallpaper_factory",
-    version="0.0.2",
+    version="0.0.4",
     install_requires=["opencv-python", "pillow"],
     entry_points={
         "console_scripts": ["wallpaper-factory = wallpaper_factory:wallpaper_factory"]

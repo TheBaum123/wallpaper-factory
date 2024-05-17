16ca3d5db1b909c9a405fa5b28aaa46610148c67
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2024-05-17 10:25:49 +0200

    add the tokyo night color scheme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = f.read()
 
 setup(
     name="wallpaper_factory",
-    version="0.0.12",
+    version="0.0.13",
     install_requires=["opencv-python", "pillow"],
     entry_points={
         "console_scripts": ["wallpaper-factory = wallpaper_factory:wallpaper_factory"]

89ea81c3dc4d4309b541ae1e92e32db547c462fc
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2024-06-04 11:17:00 +0200

    update description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = f.read()
 
 setup(
     name="wallpaper_factory",
-    version="0.0.14",
+    version="0.0.15",
     install_requires=["opencv-python", "pillow"],
     entry_points={
         "console_scripts": ["wallpaper-factory = wallpaper_factory:wallpaper_factory"]
@@ -13,4 +13,5 @@ setup(
     package_data={"": ["*.json"]},
     long_description=long_description,
     long_description_content_type="text/markdown",
+    description="This package changes images to follow some specific color schemes.",
 )

9db2b0998b688d6cdd24cddd1dec71d0cadfc6fe
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2024-06-07 11:18:47 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = f.read()
 
 setup(
     name="wallpaper_factory",
-    version="0.0.16",
+    version="0.0.17",
     install_requires=["opencv-python", "pillow"],
     entry_points={
         "console_scripts": ["wallpaper-factory = wallpaper_factory:wallpaper_factory"]
